4faf697cae740ad7b7ce7f9164492f9f51eb8d2c
Josh <joshyanworks@gmail.com>
2021-06-25 10:59:10 -0500

    modify delivery profile

--- app/services/spree_shopify_importer/connections/delivery_profile.rb
+++ app/services/spree_shopify_importer/connections/delivery_profile.rb
@@ -1,25 +1,26 @@
 module SpreeShopifyImporter
   module Connections
     class DeliveryProfile
-      def initialize(product_variant_id)
-        ShopifyAPI::Base.api_version = Spree::ShopifyImporter::Config[:shopify_api_version]
-        ShopifyAPI::Base.site = "https://#{Spree::ShopifyImporter::Config[:shopify_api_key]}:#{Spree::ShopifyImporter::Config[:shopify_password]}@#{Spree::ShopifyImporter::Config[:shopify_shop_domain]}/admin/"
-        @product_variant_id = product_variant_id
-        @DELIVERY_PROFILE_QUERY = ShopifyAPI::GraphQL.new.parse <<-'GRAPHQL'
-          query($id: ID!) {
-            productVariant (id: $id) {
-              deliveryProfile {
-                id
-                name
-                default
-              }
+      ShopifyAPI::Base.api_version = Spree::ShopifyImporter::Config[:shopify_api_version]
+      ShopifyAPI::Base.site = "https://#{Spree::ShopifyImporter::Config[:shopify_api_key]}:#{Spree::ShopifyImporter::Config[:shopify_password]}@#{Spree::ShopifyImporter::Config[:shopify_shop_domain]}/admin/"
+      DELIVERY_PROFILE_QUERY = ShopifyAPI::GraphQL.new.parse <<-'GRAPHQL'
+        query($id: ID!) {
+          productVariant (id: $id) {
+            deliveryProfile {
+              id
+              name
+              default
             }
           }
-        GRAPHQL
+        }
+      GRAPHQL
+
+      def initialize(product_variant_id)
+        @product_variant_id = product_variant_id
       end
 
       def call
-        response = ShopifyAPI::GraphQL.new.query(@DELIVERY_PROFILE_QUERY, variables: { id: product_variant_id })
+        response = ShopifyAPI::GraphQL.new.query(DELIVERY_PROFILE_QUERY, variables: { id: product_variant_id })
 
         return if response.data.product_variant.nil?
 
